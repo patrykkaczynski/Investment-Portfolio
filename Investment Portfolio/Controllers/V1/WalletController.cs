db37b86db553716dd07cf6c8a3b4b9ed1bf2110c
patrykkaczynski <patrykkaczynski@proton.me>
2023-01-03 00:48:29 +0100

    Add versioning by header

---
+++ Investment Portfolio/Controllers/V1/WalletController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Investment_Portfolio.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    [Route("api/wallet")]
+    [ApiController]
+    public class WalletController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetAll()
+        {
+            var elements = new List<string>() { "test", "test1", "test2" };
+            return Ok(elements);
+        }
+
+
+    }
+}
--- Investment Portfolio/Program.cs
+++ Investment Portfolio/Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(o =>
+{
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.ReportApiVersions = true;
+    o.ApiVersionReader = new HeaderApiVersionReader("api-version");
+});
 
 var app = builder.Build();
 
